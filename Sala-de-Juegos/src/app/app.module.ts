3091ae98a71b7d16b8454a97df5473d08a8c5058
BayonLucas <lucas.bayon31@hotmail.com>
2023-09-14 17:20:34 -0300

    Ruteo

--- Sala-de-Juegos/src/app/app.module.ts
+++ Sala-de-Juegos/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    QuienSoyComponent
+    QuienSoyComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
--- Sala-de-Juegos/src/app/components/login/login.component.ts
+++ Sala-de-Juegos/src/app/components/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
+
+
+
 export class LoginComponent {
+
+  public mail: string = '';
+  public pass: string = '';
+
+  constructor(private router: Router){}
+
+
+  public onClick(event?: any): void{
+    console.log(this.mail);
+    console.log(this.pass);
+    // this.router.navigateByUrl("Home");
+  }
+
 
 }
